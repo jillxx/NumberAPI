15c0130334f7d28a7d9dfe01deebd12976fa66f7
jillxx <jiaoxu84@gmail.com>
2018-06-13 13:08:43 -0400

    first commit

---
+++ src/main/java/com/number/Number/NumberApplication.java
@@ -0,0 +1,12 @@
+package com.number.Number;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberApplication.class, args);
+	}
+}
---
+++ src/test/java/com/number/Number/NumberApplicationTests.java
@@ -0,0 +1,16 @@
+package com.number.Number;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NumberApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
